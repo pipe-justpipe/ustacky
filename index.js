5a3cfed50e5be2ee33008d927f702e078b331735
pipe-justpipe <aremopipeloluwa@gmail.com>
2023-12-04 23:17:26 +0100

    updated script

--- index.js
+++ index.js
@@ -286,7 +286,7 @@ function payWithPaystack(e) {
     email: document.getElementById("email").value,
     amount: document.getElementsByClassName("total_amount").value * 100,
     ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
-    // label: "Optional string that replaces customer email"
+    // label: "Optional string that replaces customer email"////////////////
     onClose: function(){
       alert('Window closed.');
     },
